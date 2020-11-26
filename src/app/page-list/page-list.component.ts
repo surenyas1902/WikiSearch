6ebac4ca77d75ac8e1521de04aa894e2a344e332
Surendiran S <44779174+surenyas1902@users.noreply.github.com>
2020-11-26 19:05:41 +0530

    Search API got completed

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-list',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageListComponent implements OnInit {
 
+  @Input() pages = [];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Output() submitted = new EventEmitter<string>();
   term = '';
   constructor() { }
 
@@ -19,7 +20,7 @@ export class SearchBarComponent implements OnInit {
 
   onFormSubmit(event: KeyboardEvent) {
     event.preventDefault();
-    console.log(this.term);
+    this.submitted.emit(this.term);
   }
 
 }
